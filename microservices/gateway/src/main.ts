b099c905687405378f13f87c23306d7f5c611c6e
Phan Kien <phankien110302@gmail.com>
2025-06-03 10:31:15 +0700

    ref:project

--- microservices/gateway/src/main.ts
+++ microservices/gateway/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { ResponseInterceptor } from './configs/interceptor/custom.interceptor';
+import { ResponseInterceptor } from './interceptor/custom.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
