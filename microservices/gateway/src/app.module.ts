9e906eca25dce79bc4bdb21dbe7b6b3db9a7a0ec
Phan Kien <phankien110302@gmail.com>
2025-07-21 10:16:37 +0700

    redis caching

--- microservices/gateway/src/app.module.ts
+++ microservices/gateway/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Inject, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
@@ -6,49 +6,16 @@ import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UploadModule } from './modules/upload/upload.module';
 import { CacheModule } from '@nestjs/cache-manager';
-import * as redisStore from 'cache-manager-redis-store';
-import Redis from 'ioredis';
+import { createKeyv } from '@keyv/redis';
 @Module({
   imports: [
-    CacheModule.registerAsync({
-      isGlobal: true,
-      useFactory: async () => {
-        const redisConfig = {
-          host: process.env.REDIS_HOST,
-          port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : undefined,
-          username: process.env.REDIS_USER,
-          password: process.env.REDIS_PASSWORD,
-        };
-        console.log({ redisConfig });
-
-        await new Promise<void>((resolve, reject) => {
-          const testClient = new Redis({
-            ...redisConfig,
-            lazyConnect: true,
-            tls: {}, // Redis cloud như Redis Enterprise thường cần TLS
-          });
-
-          testClient.connect().then(() => {
-            console.log('✅ Connected to Redis successfully!');
-            testClient.disconnect();
-            resolve();
-          }).catch((err) => {
-            console.error('❌ Redis connection failed:', err.message);
-            testClient.disconnect();
-            reject(err);
-          });
-        });
-        // Trả config sau khi kết nối OK
-        return {
-          store: (await redisStore).redisStore,
-          ...redisConfig,
-          ttl: 600,
-        };
-      },
-    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      stores: [createKeyv(process.env.REDIS_URL)],
     }),
     UserModule,
     AuthModule,
